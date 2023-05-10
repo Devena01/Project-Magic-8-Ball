65adfc08fc3694beca10073c8a51a0266bd4bc84
Ana Devenish <132521746+Devena01@users.noreply.github.com>
2023-05-09 22:28:17 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,101 @@
+var userName = " ";
+
+if(userName !== " " ) {
+
+    console.log (`Hello, ${userName}!`);
+
+}else {
+    console.log ('Hello');
+}
+
+var userQuestion= " ";
+
+  console.log(`${userQuestion}, ${userName}`);
+
+ var randomNumber=Math.floor(Math.random () * 8);
+
+ console.log(randomNumber);
+
+ var eightBall= " ";
+
+ if (randomNumber === 0 ){
+
+    console.log('It is certain');
+
+ } else if (randomNumber === 1){
+
+    console.log('It is decidedly so');
+
+ } if (randomNumber === 2){
+
+    console.log('Reply hazy try again');
+
+ }else if (randomNumber === 3){
+
+    console.log('Cannot predict now');
+
+ }if (randomNumber === 4){
+
+    console.log('Do not count on it');
+
+ }else if ( randomNumber === 5){
+
+    console.log('My sources say no');
+
+ }if ( randomNumber === 6){
+
+    console.log('Outlook not good');
+
+ }else if(randomNumber === 7){
+
+    console.log('Signs point to yes');
+
+ } if(randomNumber=== 8){
+
+    console.log('No way Jose!');
+
+ }
+
+ console.log(eightBall == randomNumber );
+      
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
